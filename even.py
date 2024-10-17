683e4e22a5e8e9942c786db966073dd482fe8a32
Mahekjain10 <mahek2k4@gmail.com>
2024-10-17 03:29:54 -0700

    added

--- even.py
+++ even.py
@@ -9,3 +9,19 @@ def evensum(n):
     return sum
 n=20
 print("Sum of first ",n,"Even number is:",evensum(n))
+# exchanging the values of variables
+def exchange(x,y):
+    x,y=y,x
+    print("After exchange of x,y")
+    print("x:",x)
+    print("y:",y)
+x=int(input("Enter value of x:"))
+y=int(input("Enter value of y:"))
+print("Before exchange of x,y")
+print("x:",x)
+print("y:",y)
+exchange(x,y)
+
+
+
+
